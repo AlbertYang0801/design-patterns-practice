f8576c6fb6f1be856419277230f4e2e87ed01d8b
albert.yang <18438049166@163.com>
2020-11-27 17:57:33 +0800

    # 提交深拷贝的练习

--- design-prototype/src/main/java/com/design/practice/clone/deepclone/DeepCloneableTarget.java
+++
@@ -1,8 +0,0 @@
-package com.design.practice.clone.deepclone;
-
-/**
- * @author Albert
- * @date 2020/11/26 下午9:11
- */
-public class DeepCloneableTarget {
-}
--- design-prototype/src/main/java/com/design/practice/clone/shallowclone/SheepClient.java
+++ design-prototype/src/main/java/com/design/practice/clone/shallowclone/SheepClient.java
@@ -26,12 +26,12 @@ public class SheepClient {
         Sheep cloneSheep = sheep.clone();
         Sheep cloneSheep2 = sheep.clone();
 
-        //判断克隆出的多个对象的引用对象的hashcode值
-        System.out.println(cloneSheep.getFirend().hashCode());
-        System.out.println(cloneSheep2.getFirend().hashCode());
-
         //原型模式创建的对象，不相等。为新创建的对象
         System.out.println(cloneSheep==cloneSheep2);
+
+        //判断克隆出的多个对象的引用对象的hashcode值，表示对象内容一致
+        System.out.println(cloneSheep.getFirend().hashCode());
+        System.out.println(cloneSheep2.getFirend().hashCode());
 
         //克隆对象里的引用类型属性，在生成新对象的时候，复制的是引用类型的地址，故新创建的对象里引用类型属性是相等的。
         System.out.println(cloneSheep.getFirend()==cloneSheep2.getFirend());
