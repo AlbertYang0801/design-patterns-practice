5cf0763c4e7796dfde1f5ef13e4bfc0c49b74790
albert.yang <18438049166@163.com>
2020-12-08 15:03:52 +0800

    # 提交建造者模块

---
+++ design-builder/src/main/java/com/design/practice/BuilderApplication.java
@@ -0,0 +1,16 @@
+package com.design.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Albert
+ */
+@SpringBootApplication
+public class BuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuilderApplication.class, args);
+    }
+
+}
