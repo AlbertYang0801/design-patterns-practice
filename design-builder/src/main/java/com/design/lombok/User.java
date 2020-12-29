aab6fd81e78f9b60cd6024d33f7e72d31f271d48
albert.yang <18438049166@163.com>
2020-12-29 12:08:29 +0800

    # 提交lombok建造者模式注解实现

---
+++ design-builder/src/main/java/com/design/lombok/User.java
@@ -0,0 +1,77 @@
+package com.design.lombok;
+
+/**
+ * 实现lombok的@builder注解，建造者模式功能
+ *
+ * @author Albert
+ * @date 2020/12/29 上午11:37
+ */
+public class User {
+
+    private String id;
+    private String name;
+    private int age;
+
+    public User(final String id, final String name, final int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public static User.UserExampleBuilder builder() {
+        return new User.UserExampleBuilder();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public static class UserExampleBuilder {
+
+        private String id;
+        private String name;
+        private int age;
+
+        public User.UserExampleBuilder id(final String id) {
+            this.id = id;
+            return this;
+        }
+
+        public User.UserExampleBuilder name(final String name) {
+            this.name = name;
+            return this;
+        }
+
+        public User.UserExampleBuilder age(final int age) {
+            this.age = age;
+            return this;
+        }
+
+        public User build() {
+            return new User(this.id, this.name, this.age);
+        }
+
+        @Override
+        public String toString() {
+            return "UserExampleBuilder{" +
+                    "id='" + id + '\'' +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        User user = User.builder().id("123").name("test").age(12).build();
+        System.out.println(user);
+    }
+
+
+}
