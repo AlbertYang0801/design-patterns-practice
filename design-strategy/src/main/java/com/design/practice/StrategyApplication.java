4bf835d5df531fd07ed53f53ee22e88a1d198fa1
albert.yang <18438049166@163.com>
2021-01-20 10:39:26 +0800

    # 修改项目路径，删除parent模块

---
+++ design-strategy/src/main/java/com/design/practice/StrategyApplication.java
@@ -0,0 +1,16 @@
+package com.design.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Albert
+ */
+@SpringBootApplication
+public class StrategyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrategyApplication.class, args);
+    }
+
+}
---
+++ design-strategy/src/main/java/com/design/practice/unsolved/TakeawayPrice.java
@@ -0,0 +1,12 @@
+package com.design.practice.unsolved;
+
+/**
+ * 未优化之前
+ * @author Albert
+ * @date 2021/1/20 上午10:09
+ */
+public class TakeawayPrice {
+
+
+
+}
